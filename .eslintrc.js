b677701a30b64701c6e662b066fdddd223ca6bc7
Ken Marsh <kenskyfish@gmail.com>
2022-04-09 16:22:29 +0700

    after lint config and npx eslint . --fix

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-base",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    curly: ["error", "all"],
+    "no-console": ["warn", { allow: ["info", "warn", "error"] }],
+    "no-use-before-define": ["error", { functions: false }],
+    // "no-empty": ["error", { allowEmptyCatch: true }],
+    // "no-unsafe-finally": ["off"],
+  },
+};

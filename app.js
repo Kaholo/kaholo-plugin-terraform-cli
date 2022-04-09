436ed3209e54fae68a7b15bff4bf7f0b12d2b44c
Ken Marsh <kenskyfish@gmail.com>
2022-04-09 16:22:29 +0700

    after lint config and npx eslint . --fix

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 const { execTerraform, parseVars } = require("./helpers");
 
-async function executeTerraformInit(action, settings){
-	const args = action.params.UPGRADE ? ["-upgrade"] : [];
-	return execTerraform("init", args, action.params.PATH);
+async function executeTerraformInit(action, settings) {
+  const args = action.params.UPGRADE ? ["-upgrade"] : [];
+  return execTerraform("init", args, action.params.PATH);
 }
 
-async function executeTerraformApply(action, settings){
-	let args = parseVars(action.params.VAR || []);
-	if (action.params.VARFILE) args.push(`-var-file="${action.params.VARFILE}"`);
-	if (action.params.OTHER) args.push(action.params.OTHER);
-	args.push("--auto-approve");
+async function executeTerraformApply(action, settings) {
+  const args = parseVars(action.params.VAR || []);
+  if (action.params.VARFILE) { args.push(`-var-file="${action.params.VARFILE}"`); }
+  if (action.params.OTHER) { args.push(action.params.OTHER); }
+  args.push("--auto-approve");
 
-	return execTerraform("apply", args, action.params.PATH);
+  return execTerraform("apply", args, action.params.PATH);
 }
 
-async function executeTerraformPlan(action, settings){
-	let args = parseVars(action.params.vars);
-	if (action.params.varFile) args.push(`-var-file="${action.params.varFile}"`);
-	if (action.params.OPTIONS) args.push(action.params.OPTIONS);
-	return execTerraform("plan", args, action.params.PATH);
+async function executeTerraformPlan(action, settings) {
+  const args = parseVars(action.params.vars);
+  if (action.params.varFile) { args.push(`-var-file="${action.params.varFile}"`); }
+  if (action.params.OPTIONS) { args.push(action.params.OPTIONS); }
+  return execTerraform("plan", args, action.params.PATH);
 }
 
-async function executeTerraformDestroy(action, settings){
-	let args = parseVars(action.params.vars);
-	if (action.params.varFile) args.push(`-var-file="${action.params.varFile}"`);
-	if (action.params.options) args.push(action.params.options);
-	args.push("--auto-approve");
-	return execTerraform("destroy", args, action.params.path);
+async function executeTerraformDestroy(action, settings) {
+  const args = parseVars(action.params.vars);
+  if (action.params.varFile) { args.push(`-var-file="${action.params.varFile}"`); }
+  if (action.params.options) { args.push(action.params.options); }
+  args.push("--auto-approve");
+  return execTerraform("destroy", args, action.params.path);
 }
 
 module.exports = {
-	executeTerraformInit,
-	executeTerraformApply,
-	executeTerraformPlan,
-	executeTerraformDestroy
+  executeTerraformInit,
+  executeTerraformApply,
+  executeTerraformPlan,
+  executeTerraformDestroy,
 };

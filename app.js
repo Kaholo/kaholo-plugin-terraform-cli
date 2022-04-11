7f12e8a71622988a794853947e6a61fd2ee99e32
Ken Marsh <kenskyfish@gmail.com>
2022-04-11 15:20:06 +0700

    moved most of the action outside of the "if init mode" else

--- app.js
+++ app.js
@@ -19,13 +19,28 @@ async function executeTerraformDestroy(action, settings) {
 async function exeTf(mode, action, settings) {
   const path = action.params.workingDir || settings.workingDir;
   let args;
+
   if (mode === "init") {
     args = action.params.upgrade ? ["-upgrade"] : [];
-  } else {
-    args = parseVars(action.params.vars);
-    if (action.params.varFile) { args.push(`-var-file="${action.params.varFile}"`); }
-    if (mode === "apply" || mode === "destroy") { args.push("--auto-approve"); }
-    if (action.params.options) { args.push(action.params.options); }
+    return execTerraform(mode, args, workDir);
+  }
+
+  args = parseVars(action.params.vars);
+  if (action.params.varFile) { args.push(`-var-file="${action.params.varFile}"`); }
+  if (mode === "apply" || mode === "destroy") { args.push("--auto-approve"); }
+  if (action.params.options) { args.push(action.params.options); }
+  if (action.params.secretVarFile) {
+    const svfName = await makeVarFile(action.params.secretVarFile, workDir);
+    args.push(`-var-file="${svfName}"`);
+    // this securely deletes any files matching temp-9bxY9f-* in the working directory
+    const shredArray = [
+      "temp9bxY9fcount=$(ls | grep temp-9bxY9f- | wc -l)", // count them
+      "if [ $temp9bxY9fcount -gt 0 ]", // if any exist
+      "then shred -n 3 -z -u temp-9bxY9f-*", // shred them
+      "fi",
+    ];
+    const shredCmd = shredArray.join("; ");
+    args.push(`; ${shredCmd}`);
   }
   return execTerraform(mode, args, path);
 }

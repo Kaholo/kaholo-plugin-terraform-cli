491c23595bf2ca5966ff32e1a6b8f12468042f16
xkcm <xkcm16@gmail.com>
2022-04-28 14:37:26 +0200

    Refactor environment variables

--- terraform-cli.js
+++ terraform-cli.js
@@ -12,14 +12,18 @@ const {
 } = require("./helpers");
 const { TERRAFORM_DOCKER_IMAGE } = require("./consts.json");
 
-async function createDockerCommand({ mountTerraformDir = false, mountVariables = false }) {
+async function createDockerCommand({
+  terraformCommand,
+  mountTerraformDir = false,
+  mountVariables = false,
+}) {
   const user = await getCurrentUserId();
   return `
     docker run --rm \
     --user ${user} \
     ${mountTerraformDir ? "-v $TERRAFORM_DIR:$TERRAFORM_DIR_MOUNT_POINT" : ""} \
     ${mountVariables ? "-v $TERRAFORM_VAR_FILE:$TERRAFORM_VAR_FILE_MOUNT_POINT" : ""} \
-    ${TERRAFORM_DOCKER_IMAGE} $TERRAFORM_COMMAND
+    ${TERRAFORM_DOCKER_IMAGE} ${terraformCommand}
   `.trim();
 }
 
@@ -33,10 +37,10 @@ function createTerraformCommand(baseCommand, {
   const postArgs = [...additionalArgs, "-no-color"];
   const preArgs = [];
   if (workingDirectory) {
-    preArgs.push(`-chdir=${workingDirectory}`);
+    preArgs.push("-chdir=$TERRAFORM_DIR_MOUNT_POINT");
   }
   if (variableFile) {
-    postArgs.push(`-var-file=${variableFile}`);
+    postArgs.push("-var-file=$TERRAFORM_VAR_FILE_MOUNT_POINT");
   }
   if (json && isJsonAllowed(command)) {
     postArgs.push("-json");
@@ -65,17 +69,17 @@ async function execute({
   }
 
   const terraformCommand = createTerraformCommand(command, {
-    workingDirectory: environmentVariables.get("TERRAFORM_DIR_MOUNT_POINT"),
-    variableFile: environmentVariables.get("TERRAFORM_VAR_FILE_MOUNT_POINT"),
+    workingDirectory: environmentVariables.has("TERRAFORM_DIR_MOUNT_POINT"),
+    variableFile: environmentVariables.has("TERRAFORM_VAR_FILE_MOUNT_POINT"),
     json: !rawOutput,
     additionalArgs,
   });
-  environmentVariables.set("TERRAFORM_COMMAND", terraformCommand);
   logToActivityLog(`Generated Terraform command: ${terraformCommand}`);
 
   const dockerCommand = await createDockerCommand({
     mountTerraformDir: Boolean(workingDirectory),
     mountVariables: Boolean(variables),
+    terraformCommand,
   });
 
   let result;
